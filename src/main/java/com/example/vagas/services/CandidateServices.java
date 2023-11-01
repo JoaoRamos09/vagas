0d8dce529ddca72e7f60a9c9fcc4386cc992c3f1
Joao Ramos <joaorsramos@hotmail.com>
2023-11-01 17:36:13 -0300

    att 1: CandidateServices

--- src/main/java/com/example/vagas/services/CandidateServices.java
+++ src/main/java/com/example/vagas/services/CandidateServices.java
@@ -1,10 +1,12 @@
 package com.example.vagas.services;
 
 import com.example.vagas.models.Candidate;
-import com.example.vagas.models.ICandidateRepository;
-import com.example.vagas.models.RequestCandidate;
+import com.example.vagas.repository.ICandidateRepository;
+import com.example.vagas.dtos.CandidateDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CandidateServices {
@@ -14,6 +16,16 @@ public class CandidateServices {
 
     public void createNewCandidate(Candidate candidate){
         this.iCandidateRepository.save(candidate);
+    }
+
+    public void allSetters(Candidate candidate, CandidateDTO requestCandidate){
+        candidate.setName(requestCandidate.name());
+        candidate.setSalary_estimate(requestCandidate.salary_estimate());
+        candidate.setCPF(requestCandidate.CPF());
+    }
+
+    public List<Candidate> getAllCandidates(){
+      return iCandidateRepository.findAll();
     }
 
 }
