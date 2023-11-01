e2a13620ca4ba7bd959ada3f8f99420a50d4f263
Joao Ramos <joaorsramos@hotmail.com>
2023-11-01 17:35:02 -0300

    feat: CandidateDTO

--- src/main/java/com/example/vagas/dtos/CandidateDTO.java
+++ src/main/java/com/example/vagas/dtos/CandidateDTO.java
@@ -1,15 +1,20 @@
-package com.example.vagas.models;
+package com.example.vagas.dtos;
 
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.NonNull;
 
-public record RequestCandidate(
+public record CandidateDTO(
         @NotBlank(message = "Não é permitido que esteja em branco/vazio")
         String name,
 
         @NotNull(message = "Não é permitido que esteja vazio")
-        Double salary_estimate) {
+        Double salary_estimate,
+
+        String CPF)
+
+
+{
+
 
 }
