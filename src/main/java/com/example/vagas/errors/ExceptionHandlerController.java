524cd40c687580147a1e70bc0a76427ef756e380
Joao Ramos <joaorsramos@hotmail.com>
2023-11-01 17:37:13 -0300

    feat: exceptionhandler controller and dto

---
+++ src/main/java/com/example/vagas/errors/ExceptionHandlerController.java
@@ -0,0 +1,43 @@
+package com.example.vagas.errors;
+
+import com.example.vagas.dtos.CandidateDTO;
+import com.example.vagas.dtos.ExceptionDTO;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ExceptionDTO> userNotFound(NoSuchElementException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário não foi encontrado","400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionDTO> invalidCPF(ConstraintViolationException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("CPF inválido","400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ExceptionDTO> duplicateUser(DataIntegrityViolationException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("CPF já cadastrado","400");
+    return ResponseEntity.badRequest().body(exceptionDTO);
+}
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ExceptionDTO> userNotFound(MethodArgumentTypeMismatchException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO("Usuário não foi encontrado","400");
+        return ResponseEntity.badRequest().body(exceptionDTO);
+
+    }
+
+}
