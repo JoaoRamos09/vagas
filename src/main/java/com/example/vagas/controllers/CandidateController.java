ad9a61a1963deb84933295d17ca511c58acd0ec9
Joao Ramos <joaorsramos@hotmail.com>
2023-11-01 17:35:42 -0300

    att 1: CandidateController

--- src/main/java/com/example/vagas/controllers/CandidateController.java
+++ src/main/java/com/example/vagas/controllers/CandidateController.java
@@ -1,15 +1,16 @@
 package com.example.vagas.controllers;
 
 import com.example.vagas.models.Candidate;
-import com.example.vagas.models.ICandidateRepository;
-import com.example.vagas.models.RequestCandidate;
+import com.example.vagas.repository.ICandidateRepository;
+import com.example.vagas.dtos.CandidateDTO;
+import com.example.vagas.services.CandidateServices;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -20,23 +21,34 @@ public class CandidateController {
     @Autowired
     private ICandidateRepository candidateRepository;
 
+    @Autowired
+    private CandidateServices candidateServices;
+
     @PostMapping("/")
-    public ResponseEntity<String> createNewCandidate(@RequestBody @Valid RequestCandidate requestCandidate){
+    public ResponseEntity<String> createNewCandidate(@RequestBody @Valid CandidateDTO requestCandidate){
         Candidate newCandidate = new Candidate(requestCandidate);
        candidateRepository.save(newCandidate);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body("Usuário criado com sucesso");
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity updateCandidate(@RequestBody @Valid RequestCandidate requestCandidate, @PathVariable UUID id){
+    public ResponseEntity<Candidate> updateCandidate(@RequestBody @Valid CandidateDTO candidateDTO, @PathVariable UUID id) throws Exception {
         Optional<Candidate> newCandidate = candidateRepository.findById(id);
-        if (newCandidate.isPresent()){
-            Candidate candidateSave = newCandidate.get();
-            candidateSave.setName(requestCandidate.name());
-            candidateSave.setSalary_estimate(requestCandidate.salary_estimate());
-            return ResponseEntity.ok(candidateSave);
-       }
-        return ResponseEntity.badRequest().build();
+        Candidate candidate = newCandidate.orElseThrow();
+        candidateServices.allSetters(candidate,candidateDTO);
+        return ResponseEntity.ok().body(candidate);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<Candidate>> allCandidates(){
+        List<Candidate> allCandidates = candidateServices.getAllCandidates();
+        return ResponseEntity.ok().body(allCandidates);
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity deleteCandidate(@RequestBody @Valid UUID uuid){
+        candidateRepository.deleteById(uuid);
+        return ResponseEntity.ok().body("Usuário deletado com sucesso");
     }
 }
