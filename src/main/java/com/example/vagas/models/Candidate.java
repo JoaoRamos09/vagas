6c8f57edc80e72e773ea69e4e2e77fdbe59413ba
Joao Ramos <joaorsramos@hotmail.com>
2023-11-01 17:36:24 -0300

    att 1: CandidateModel

--- src/main/java/com/example/vagas/models/Candidate.java
+++ src/main/java/com/example/vagas/models/Candidate.java
@@ -1,10 +1,12 @@
 package com.example.vagas.models;
 
+import com.example.vagas.dtos.CandidateDTO;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.UUID;
 
@@ -20,11 +22,16 @@ public class Candidate {
 
     private String name;
 
+    @Column(unique = true)
+    @CPF
+    private String CPF;
+
     private Double salary_estimate;
 
-    public Candidate(RequestCandidate requestCandidate){
+    public Candidate(CandidateDTO requestCandidate){
         this.name = requestCandidate.name();
         this.salary_estimate = requestCandidate.salary_estimate();
+        this.CPF = requestCandidate.CPF();
     }
 
 }
