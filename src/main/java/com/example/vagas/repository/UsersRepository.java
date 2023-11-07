372094d6e0eb2f82f28863b7ab64f8e062f95ed6
João Vítor Ramos <136603790+JoaoRamos09@users.noreply.github.com>
2023-11-07 02:55:58 -0300

    feat: security config

---
+++ src/main/java/com/example/vagas/repository/UsersRepository.java
@@ -0,0 +1,13 @@
+package com.example.vagas.repository;
+
+import com.example.vagas.models.UserRole;
+import com.example.vagas.models.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.UUID;
+
+public interface UsersRepository extends JpaRepository<Users, UUID> {
+    public UserDetails findByUsername(String user);
+}
